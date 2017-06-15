69c3b6d0a8331afeda01532397c65775e52fcd8d
App Academy Student <mizzou_fan10@hotmail.com>
2017-06-15 13:31:04 -0700

    Partly done with part 3

--- movie_buff/03_queries.rb
+++ movie_buff/03_queries.rb
@@ -2,6 +2,11 @@ def what_was_that_one_with(those_actors)
   # Find the movies starring all `those_actors` (an array of actor names).
   # Show each movie's title and id.
 
+  Movie.select(:id, :title)
+        .joins(:actors)
+        .where(actors: { name: those_actors })
+        .group("movies.id")
+        .having("COUNT(actors.id) >= ?", those_actors.length)
 end
 
 def golden_age
@@ -13,11 +18,21 @@ def costars(name)
   # List the names of the actors that the named actor has ever appeared with.
   # Hint: use a subquery
 
+  movie_ids = Movie.joins(:actors)
+        .where("actors.name = ?", name)
+        .pluck(:id)
+
+  Actor.joins(:movies)
+        .where("actors.name != ? AND movies.id IN (?)", name, movie_ids)
+        .distinct
+        .pluck(:name)
 end
 
 def actor_out_of_work
   # Find the number of actors in the database who have not appeared in a movie
-
+  Actor.joins("LEFT JOIN castings ON actors.id = castings.actor_id")
+        .where("castings.movie_id IS NULL")
+        .pluck(:id).length
 end
 
 def starring(whazzername)
@@ -25,8 +40,14 @@ def starring(whazzername)
   # A name is like whazzername if the actor's name contains all of the letters in whazzername,
   # ignoring case, in order.
 
-  # ex. "Sylvester Stallone" is like "sylvester" and "lester stone" but not like "stallone sylvester" or "zylvester ztallone"
-
+  # ex. "Sylvester Stallone" is like "sylvester" and "lester stone" but
+  # not like "stallone sylvester" or "zylvester ztallone"
+  match = whazzername.chars
+  match.delete(" ")
+  match = '%' + match.join('%') + '%'
+  #p match = '%' + whazzername.chars.join('%') + '%'
+  Movie.joins(:actors)
+        .where("actors.name LIKE ?", match)
 end
 
 def longest_career
